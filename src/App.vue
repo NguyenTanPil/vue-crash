e862cbd7316088d7f6c9be94e7c40ac3d3940c6a
NguyenTanPil <traila1008@gmail.com>
2024-04-20 23:31:28 +0700

    Init ui

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <AppHeaders />
+  <div class="container">
+    <AppBalance />
+    <IncomeExpenses />
+    <TransactionList :transactions="transactions" />
+    <AddTransaction />
+  </div>
+</template>
+
+<script setup>
+import AppHeaders from './components/AppHeaders.vue';
+import AppBalance from './components/AppBalance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+import { ref } from 'vue';
+
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: 299.97 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+]);
+console.log({ transactions });
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="{ minus: transaction.amount < 0, plus: transaction.amount > 0 }"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  transactions: {
+    type: Array,
+    required: true
+  }
+});
+</script>
